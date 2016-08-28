c669fd781802e689f910caab488dc3388d71b25c
Gianfranco <gianfrancomanganiello@hotmail.com>
2016-08-28 08:55:17 -0400

    Solucionado el problema de los registros

--- agregar_loteria.php
+++ agregar_loteria.php
@@ -1,17 +1,15 @@
 <?php
   require("conex.php");
   $Nombre_loteria = $_POST['nombre'];
-  mysqli_query($enlace, "INSERT INTO loterias ('Nombre_loteria') VALUES ($Nombre_loteria)");
-  mysqli_query($enlace, "INSERT INTO datos_2dig ('N_loteria') VALUES ($Nombre_loteria)");
-  mysqli_query($enlace, "INSERT INTO datos_2dig ('ultimo_ingreso_2dig') VALUES (0)");
+  mysqli_query($enlace, "INSERT INTO loterias (Nombre_loteria) VALUES ('$Nombre_loteria')") or die("q1".mysqli_error($enlace));
+  mysqli_query($enlace, "INSERT INTO datos_2dig (N_loteria, ultimo_ingreso_2dig) VALUES ('$Nombre_loteria',0)") or die("q2".mysqli_error($enlace));
   for ($i=1; $i <= 100; $i++) {
-    $sql = "INSERT INTO datos_2dig ('dig". $i ."') VALUES (0)";
+    $sql = "INSERT INTO datos_2dig (dig". $i .") VALUES (0)";
     mysqli_query($enlace, $sql);
   }
-  mysqli_query($enlace, "INSERT INTO datos_3dig ('N_loteria') VALUES ($Nombre_loteria)");
-  mysqli_query($enlace, "INSERT INTO datos_3dig ('ultimo_ingreso_2dig') VALUES (0)");
+  mysqli_query($enlace, "INSERT INTO datos_3dig (N_loteria, ultimo_ingreso_3dig) VALUES ('$Nombre_loteria',0)") or die("q3".mysqli_error($enlace));
   for ($i=1; $i <= 1000; $i++) {
-    $sql = "INSERT INTO datos_2dig ('dig". $i ."') VALUES (0)";
+    $sql = "INSERT INTO datos_2dig (dig". $i .") VALUES (0)";
     mysqli_query($enlace, $sql);
   }
   header("Location: index.php");
